2d8b198338c2cdcf98eb0df299a5520e9f8a4966
0xRAM-bit <vinyasshanker90@gmail.com>
2025-07-11 18:49:31 +0530

    v1.2

--- payload_finder/payload_finder.py
+++ payload_finder/payload_finder.py
@@ -51,11 +51,24 @@ def load_payload_file(keyword):
         path = "xss/csp_bypass.json"
     elif keyword == "generic":
         path = "xss/generic.json"
-    # else:
-    #     path = os.path.join("payloads", f"{keyword}.json")
-    with open(path, 'r') as f:
-        data = json.load(f)
-        return data["payloads"]
+    elif keyword == "sqli_generic":
+        path = "sqli/generic.json"
+    #---------------SQL Injection---------------------------------
+    elif keyword == "mysql":
+        path = "sqli/mysql.json"
+    elif keyword == "postgres":
+        path = "sqli/postgres.json"
+    elif keyword == "generic":
+        path = "sqli/generic.json"
+    else:
+        path = f"payloads/{keyword}.json"
+    try:
+        with open(path, 'r') as f:
+            data = json.load(f)
+            return data["payloads"]
+    except FileNotFoundError:
+        #print(f"No payload file found for '{keyword}' (tried: {path})")
+        return []
 
 def main():
     parser = argparse.ArgumentParser()
@@ -68,27 +81,56 @@ def main():
     parser.add_argument('-t','--tech', help='Technology or subtype (angularjs, filter_bypass, …)')
     args = parser.parse_args()
 
-    if args.payload == "filter_bypass" or args.tech == "filter_bypass":
-        payls = load_payload_file("filter_bypass")
-    elif args.payload == "polyglot":
-        payls = load_payload_file("polyglot")
-    elif args.payload == "WAF_bypass":
-        payls = load_payload_file("WAF_bypass")
-    elif args.payload == "csp_bypass":
-        payls = load_payload_file("csp_bypass")
-    elif args.payload == "generic":
-        payls = load_payload_file("generic")
-    elif args.tech:
-        payls = load_payload_file(args.tech)
+    #-----------------------------------------------------XSS -------------------------------------------------------------
+    if args.payload == "xss":
+        if args.tech == "filter_bypass":
+            payls = load_payload_file("filter_bypass")
+        elif args.tech == "polyglot":
+            payls = load_payload_file("polyglot")
+        elif args.tech == "WAF_bypass":
+            payls = load_payload_file("WAF_bypass")
+        elif args.tech == "csp_bypass":
+            payls = load_payload_file("csp_bypass")
+        elif args.tech == "generic":
+            payls = load_payload_file("generic")
+        elif args.tech:
+            payls = load_payload_file(args.tech)
+        else:
+            payls = load_payload_file("xss")
     else:
         payls = load_payload_file(args.payload)
+   #-----------------------------SQL Injection --------------------------------------------------------------------------- 
+    if args.payload == "sqli":
+        if args.tech == "mysql":
+            payls = load_payload_file("mysql")
+        elif args.tech == "postgres":
+            payls = load_payload_file("postgres")
+        elif args.tech == "generic":
+            payls = load_payload_file("sqli_generic")
+
+
+        #---------------For .txt files--------------------------------------------------------------------------------
+        if args.tech == "auth_bypass":
+            path_sql = "sqli/auth_bypass.txt"
+        if args.tech == "time_based":
+            path_sql = "sqli/time_based.txt"
+        if args.tech == "polyglot":
+            path_sql = "sqli/SQLi_Polyglots.txt"
+        
+        with open(path_sql, "r") as f:
+            data = f.readlines()
+        for i in data:
+            payload = i.strip()
+            if payload:
+                print(f"\033[92m[{args.tech}] \033[91m[{payload}]\033[0m")
+    #-------------------------------------------------------------------------------------------------------------------------
 
     if not payls or len(payls) == 0:
-        print("No payloads found.")
+        #print("No payloads found.")
         return
 
     for p in payls:
-        print(f"\033[92m[{p.get('type', '?')}] \033[91m{p.get('payload', '?')}\033[0m")
+        print(f"\033[92m[{p.get('type', '?')}] \033[91m[{p.get('payload', '?')}]\033[0m")
 
 if __name__ == "__main__":
     main()
